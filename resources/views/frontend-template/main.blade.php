456863b2e60e9a07d8834ef8a6e6b7656f2e783a
liudaniel2201@gmail.com <liudaniel2201@gmail.com>
2022-10-20 12:58:31 +0700

    perubahan frontend plus login

--- resources/views/frontend-template/main.blade.php
+++ resources/views/frontend-template/main.blade.php
@@ -360,8 +360,8 @@
         <div class="container-xxl py-6">
             <div class="container">
                 <div class="mx-auto text-center wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                    <div class="d-inline-block border rounded-pill text-primary px-4 mb-3">Testimonial</div>
-                    <h2 class="mb-5">What Our Clients Say!</h2>
+                    <div class="d-inline-block border rounded-pill text-primary px-4 mb-3">Alumni</div>
+                    <h2 class="mb-5">What Our Alumni CKTC Say!</h2>
                 </div>
                 <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
                     <div class="testimonial-item rounded p-4">
